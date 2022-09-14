28dbdc0936a2b85c5dfa625b093db2101b5c2a7c
YaneliSantillan <111669552+YaneliSantillan@users.noreply.github.com>
2022-09-14 06:37:51 -0500

    Add files via upload

---
+++ Consola.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacturacionElectronicaCompleta
+{
+    internal class Consola
+    {
+        public ConsoleColor colorTextoDeaulft;
+
+        public Consola(ConsoleColor _colorTextoDeaulft)
+        {
+            this.colorTextoDeaulft = _colorTextoDeaulft;
+        }
+
+        public void Marco(int x, int y, int x2, int y2)
+        {
+            Escribir(x, y, ConsoleColor.DarkRed, "╔");
+            Escribir(x2, y2, ConsoleColor.DarkRed, "╝");
+            Escribir(x, y2, ConsoleColor.DarkRed, "╚");
+            Escribir(x2, y, ConsoleColor.DarkRed, "╗");
+
+            for (int i = x + 1; i < x2; i++)
+            {
+                Escribir(i, y, ConsoleColor.DarkYellow, "═");
+                Escribir(i, y2, ConsoleColor.DarkYellow, "═");
+            }
+
+            for (int i = y + 1; i < y2; i++)
+            {
+                Escribir(x, i, ConsoleColor.DarkMagenta, "║");
+                Escribir(x2, i, ConsoleColor.DarkMagenta, "║");
+            }
+
+        }
+        public void Escribir(int left, int top, ConsoleColor color, String Texto)
+        {
+            Console.SetCursorPosition(left, top);
+            Console.ForegroundColor = color;
+            Console.Write(Texto);
+            Console.ForegroundColor = colorTextoDeaulft;
+        }
+
+        public String leerCadena(int left, int top, int longitud = 0)
+        {
+            String valor = "";
+            do
+            {
+                Console.SetCursorPosition(left, top); Console.Write("                    ");
+                Console.SetCursorPosition(left, top);
+                valor = Console.ReadLine();
+
+                if (longitud > 0)
+                {
+                    if (valor.Length != longitud) valor = "";
+                }
+
+            } while (valor == "");
+
+            return valor;
+        }
+
+        public int leerNumeroEntero(int left, int top)
+        {
+            int valor;
+            do
+            {
+                Console.SetCursorPosition(left, top); Console.Write("                    ");
+                Console.SetCursorPosition(left, top);
+            } while (!int.TryParse(Console.ReadLine(), out valor));
+
+            return valor;
+        }
+
+        public double leerNumeroDecimal(int left, int top)
+        {
+            double valor;
+            do
+            {
+                Console.SetCursorPosition(left, top); Console.Write("                    ");
+                Console.SetCursorPosition(left, top);
+            } while (!double.TryParse(Console.ReadLine(), out valor));
+
+            return valor;
+        }
+
+        public void MenuPrincipal()
+        {
+
+            Escribir(35, 1, ConsoleColor.Yellow, "MENÚ DE OPCIONES");
+            Escribir(36, 2, ConsoleColor.DarkGreen, "!!BIENVENIDO!!");
+            Escribir(32, 4, ConsoleColor.DarkMagenta, "1.- ");
+            Escribir(35, 4, ConsoleColor.White, "PRODUCTOS");
+            Escribir(32, 5, ConsoleColor.DarkMagenta, "2.- ");
+            Escribir(35, 5, ConsoleColor.White, "CLIENTES");
+            Escribir(32, 6, ConsoleColor.DarkMagenta, "3.- ");
+            Escribir(35, 6, ConsoleColor.White, "PROVEEDORES");
+            Escribir(32, 7, ConsoleColor.DarkMagenta, "4.- ");
+            Escribir(35, 7, ConsoleColor.White, "EMPLEADOS");
+            Escribir(32, 8, ConsoleColor.DarkMagenta, "5.- ");
+            Escribir(35, 8, ConsoleColor.White, "FACTURAR");
+            Escribir(32, 9, ConsoleColor.DarkMagenta, "6.- ");
+            Escribir(35, 9, ConsoleColor.White, "SALIR..");
+            Marco(25, 3, 65, 11);
+
+        }
+
+        public void MenuItems(String ItemDescripcion)
+        {
+
+            Escribir(35, 1, ConsoleColor.Yellow, "MENÚ DE OPCIONES DE " + ItemDescripcion);
+            Escribir(32, 4, ConsoleColor.Yellow, "1.- ");
+            Escribir(35, 4, ConsoleColor.White, "AGREGAR " + ItemDescripcion);
+            Escribir(32, 5, ConsoleColor.Yellow, "2.- ");
+            Escribir(35, 5, ConsoleColor.White, "VER " + ItemDescripcion);
+            Escribir(32, 6, ConsoleColor.Yellow, "3.- ");
+            Escribir(35, 6, ConsoleColor.White, "ACTUALIZAR " + ItemDescripcion);
+            Escribir(32, 7, ConsoleColor.Yellow, "4.- ");
+            Escribir(35, 7, ConsoleColor.White, "MOSTRAR ITEMS.." + ItemDescripcion);
+            Escribir(32, 8, ConsoleColor.Yellow, "5.- ");
+            Escribir(35, 8, ConsoleColor.White, "ELIMINAR " + ItemDescripcion);
+            Escribir(32, 9, ConsoleColor.Yellow, "6.- ");
+            Escribir(35, 9, ConsoleColor.White, "REGRESAR AL MENÚ PRINCIPAL..");
+            Marco(25, 3, 65, 11);
+
+        }
+
+
+        public void MenuFacturación()
+        {
+
+            Escribir(35, 1, ConsoleColor.Yellow, "MENÚ DE OPCIONES DE FACTURACIÓN ");
+            Escribir(32, 4, ConsoleColor.Yellow, "1.- ");
+            Escribir(35, 4, ConsoleColor.White, "CREAR FACTURA ");
+            Escribir(32, 5, ConsoleColor.Yellow, "2.- ");
+            Escribir(35, 5, ConsoleColor.White, "BUSCAR FACTURA ");
+            Escribir(32, 6, ConsoleColor.Yellow, "3.- ");
+            Escribir(35, 6, ConsoleColor.White, "MOSTRAR FACTURAS..");
+            Escribir(32, 7, ConsoleColor.Yellow, "4.- ");
+            Escribir(35, 7, ConsoleColor.White, "ELIMINAR FACTURA ");
+            Escribir(32, 8, ConsoleColor.Yellow, "5.- ");
+            Escribir(35, 8, ConsoleColor.White, "REGRESAR AL MENÚ PRINCIPAL..");
+            Marco(25, 3, 65, 11);
+
+        }
+        public void PintarFondo(ConsoleColor color)
+        {
+            Console.BackgroundColor = color;
+            for (int i = 0; i <= 50; i++)
+                for (int j = 0; j <= 15; j++)
+                {
+                    Console.SetCursorPosition(i, j);
+                    Console.Write(" ");
+                }
+
+
+        }
+
+
+        public int leerOpcion()
+
+        {
+            int opcion = 0;
+            do
+            {
+                Escribir(25, 12, ConsoleColor.DarkCyan, "Ingrese una opción: ");
+                opcion = leerNumeroEntero(45, 12);
+            } while (opcion <= 0 || opcion > 6);
+
+            return opcion;
+        }
+    }
+}
---
+++ Proveedores.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace FacturacionElectronicaCompleta
+{
+    internal class Proveedores : Item
+    {
+        
+        public String Ruc;
+        public String RazonSocial;
+        public String Direccion;
+        public String Ciudad;
+        public String Telefono;
+
+        public Proveedores(Consola _consola, BaseDatos _objBD) : base(_consola, _objBD, "000", "Proveedores", "cedula")
+        {
+        }
+
+        public Proveedores(Consola _consola, BaseDatos _objBD, String _Codigo,
+            String Ruc, String RazonSocial, String Direccion, String Ciudad, String Telefono)
+            : base(_consola, _objBD, _Codigo, "Proveedor", "cedula")
+        {
+            this.Ruc = Ruc;
+            this.RazonSocial = RazonSocial;
+            this.Direccion = Direccion;
+            this.Ciudad = Ciudad;
+            this.Telefono = Telefono;
+        }
+        public override Item creatItem(Consola _consola, BaseDatos _objBD)
+        {
+            return new Proveedores(_consola, _objBD);
+        }
+        public override Item creatItem(Consola _consola, BaseDatos _objBD, DataRow _registro)
+        {
+            return new Proveedores(_consola, _objBD, _registro["Cedula"].ToString(),
+                (_registro["RUC"].ToString()), _registro["RazonSocial"].ToString(), _registro["Direccion"].ToString(),
+                _registro["Ciudad"].ToString(), (_registro["Telefono"].ToString()));
+        }
+        public override void mostrarMembreteTabla()
+        {
+            consola.Escribir(40, 2, ConsoleColor.Yellow, "LISTA DE PROVEEDORES");
+            consola.Escribir(5, 5, ConsoleColor.Blue, "N°"); consola.Escribir(10, 5, ConsoleColor.Blue, "Cédula");
+            consola.Escribir(30, 5, ConsoleColor.Blue, "RUC");consola.Escribir(60, 5, ConsoleColor.Blue, "Razon Social");
+            consola.Escribir(70, 5, ConsoleColor.Blue, "Direccion"); consola.Escribir(80, 5, ConsoleColor.Blue, "Ciudad");
+            consola.Escribir(120, 5, ConsoleColor.Blue, "Telefono");
+            consola.Marco(3, 4, 95, 15);
+        }
+        public override void mostrarInfoComoFila(int Num, int fila)
+        {
+            consola.Escribir(5, fila, ConsoleColor.White, Num.ToString());
+            consola.Escribir(10, fila, ConsoleColor.White, Ruc);
+            consola.Escribir(25, fila, ConsoleColor.White, RazonSocial);
+            consola.Escribir(55, fila, ConsoleColor.White, Direccion);
+            consola.Escribir(70, fila, ConsoleColor.White, Ciudad);
+            consola.Escribir(80, fila, ConsoleColor.White, Telefono.ToString());
+        }
+        public override void mostrarInfo()
+        {
+
+            consola.Escribir(30, 2, ConsoleColor.Red, "INFORMACIÓN DEL EMPLEADO");
+            consola.Marco(10, 3, 65, 11);
+            consola.Escribir(20, 5, ConsoleColor.Yellow, "Cédula: "); consola.Escribir(35, 5, ConsoleColor.White, Codigo);
+            consola.Escribir(20, 6, ConsoleColor.Yellow, "RUC: "); consola.Escribir(35, 6, ConsoleColor.White, Ruc.ToString());
+            consola.Escribir(20, 7, ConsoleColor.Yellow, "Razon Social: "); consola.Escribir(35, 7, ConsoleColor.White, RazonSocial);
+            consola.Escribir(20, 8, ConsoleColor.Yellow, "Direccion: "); consola.Escribir(35, 8, ConsoleColor.White, Direccion);
+            consola.Escribir(20, 9, ConsoleColor.Yellow, "Ciudad: "); consola.Escribir(35, 8, ConsoleColor.White, Ciudad);
+            consola.Escribir(20, 10, ConsoleColor.Yellow, "Telefono: "); consola.Escribir(35, 8, ConsoleColor.White, Telefono.ToString());
+        }
+
+        public override void leerInfo()
+        {
+
+            consola.Escribir(30, 2, ConsoleColor.Red, "NUEVO PROVEEDOR");
+            consola.Marco(10, 3, 65, 11);
+            consola.Escribir(20, 5, ConsoleColor.Yellow, "Cédula: ");
+            consola.Escribir(20, 6, ConsoleColor.Yellow, "RUC: ");
+            consola.Escribir(20, 7, ConsoleColor.Yellow, "Razon Social: ");
+            consola.Escribir(20, 8, ConsoleColor.Yellow, "Direccion: ");
+            consola.Escribir(20, 9, ConsoleColor.Yellow, "Ciudad: ");
+            consola.Escribir(20, 10, ConsoleColor.Yellow, "Telefono: ");
+            Codigo = consola.leerCadena(35, 5);
+            Ruc =consola.leerCadena(35, 6);
+            RazonSocial = consola.leerCadena(35, 7);
+            Direccion = consola.leerCadena(35, 8);
+            Ciudad = consola.leerCadena(35, 9);
+            Telefono = consola.leerCadena(35, 10);
+
+        }
+
+
+
+        public override String getSQL(String TipoSQL, String CodigoABuscar = "")
+        {
+            String SQL = "";
+            switch (TipoSQL)
+            {
+                case "Insert":
+                    SQL = "Insert into TbProveedores (Ruc, RazonSocial,Direccion,Ciudad,Telefono) values('"
+                       + Ruc + "','" + RazonSocial + "','" + Direccion + "','" + Ciudad + "','" + Telefono + "');";
+                    break;
+                case "Delete":
+                    SQL = "Delete from TbProveedores where Ruc='" + CodigoABuscar + "'";
+                    break;
+                case "Select":
+                    if (CodigoABuscar != "")
+                    {
+                        SQL = "Select * from TbProveedores where Ruc='" + CodigoABuscar + "'";
+                    }
+                    else
+                    {
+                        SQL = "Select * from TbProveedores order by Ruc";
+                    }
+                    break;
+            }
+
+            return SQL;
+        }
+    }
+}
